5095c804465be7518ad7642841f4c1770f6db6e7
Samuel Lampa <samuel.lampa@gmail.com>
2015-12-16 17:03:52 +0100

    Added first test: Accessing the Dashboard with a session

--- sftp_beamer/sftp_beamer/test_views.py
+++ sftp_beamer/sftp_beamer/test_views.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.test.client import RequestFactory
+from django.contrib.sessions.middleware import SessionMiddleware
+from sftp_beamer.views import DashboardView
 
 # Create your tests here.
+
+class DashboardViewTests(TestCase):
+
+    def setUp(self):
+        self.request_factory = RequestFactory()
+        self.view = DashboardView()
+        self.session_middleware = SessionMiddleware()
+
+    def test_get_with_a_session(self):
+        request = self.request_factory.get('/sftp_beamer/dashboard')
+        self.session_middleware.process_request(request)
+        request.session.save()
+        response = self.view.get(request)
+        self.assertEqual(response.status_code, 200)
